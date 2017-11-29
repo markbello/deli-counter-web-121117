13ff7f90f8d98ba0b5639df75e996b2d1b848e07
Mark Bello <mcbello51286@gmail.com>
2017-11-29 15:42:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,9 @@
-current_line = []
-
-def line
-  if current_line.count == 0
+def line(katz_deli)
+  if katz_deli.count == 0
     puts "The line is currently empty."
   else
     phrase = "The line is currently:"
-    current_line.each_with_index{|name, i| phrase.concat(" #{i+1}. #{name}")}
+    katz_deli.each_with_index{|name, i| phrase.concat(" #{i+1}. #{name}")}
     puts phrase
   end
 end
